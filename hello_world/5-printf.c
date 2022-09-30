651c7f1ee226116c094044e7b4710495569cf838
CyrineZekri <cyrinezekri4@gmail.com>
2022-09-30 09:15:48 -0700

    use

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
-int main(void) {
+/**
+ * main -prints .
+ * Return : 0
+ */
+int main(void)
+{
 printf("with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
