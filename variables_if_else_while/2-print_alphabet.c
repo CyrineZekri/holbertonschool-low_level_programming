bb258574d41c7eb1e01bc9afaf4e45853e676640
CyrineZekri <cyrinezekri4@gmail.com>
2022-10-03 18:56:58 +0100

    3rd

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -prints.
+ * Return:0
+ */
+int main(void)
+{
+    char c;
+
+    for (c == 'a'; c == 'z', ++c)
+    {
+        putchar("% c", c);
+    }
+    return (0);
+}
