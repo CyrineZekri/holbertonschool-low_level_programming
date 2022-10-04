a0adc3d4f21846048e283ccb56df74ae4572f816
CyrineZekri <cyrinezekri4@gmail.com>
2022-10-04 22:29:31 +0100

    4th

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -6,11 +6,11 @@
 int main(void)
 {
 int i;
-for (i < 97 ; i < 102 ; ++i)
+for (i = 97 ; i < 123 ; ++i)
 {
 putchar(i);
 }
-for (i > 25 ; i < 100 ; ++i)
+for (i = 65 ; i < 91 ; ++i)
 {
 putchar(i);
 }
