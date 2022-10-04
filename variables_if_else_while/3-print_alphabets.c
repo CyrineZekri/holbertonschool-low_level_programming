9a5e8aec5dbc8a1e5cd91788082ef91bb5ca851d
CyrineZekri <cyrinezekri4@gmail.com>
2022-10-04 22:26:31 +0100

    4th

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+*main-prints.
+*Return:0
+*/
+int main(void)
+{
+int i;
+for (i < 97 ; i < 102 ; i++)
+{
+putchar(i);
+}
+for (i > 25 ; i < 100 ; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
+
