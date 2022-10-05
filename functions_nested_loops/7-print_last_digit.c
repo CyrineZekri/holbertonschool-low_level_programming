42fd37d8437f0021c48e5f6f5b1b48dc939c334f
CyrineZekri <cyrinezekri4@gmail.com>
2022-10-05 23:29:18 +0100

    last commit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *print_last_digit - returns the last digit of a number.
+ *@i:variable input
+ *Return: last digit.
+ */
+int print_last_digit(int i)
+{
+    int j = i % 10;
+    return (j);
+}
