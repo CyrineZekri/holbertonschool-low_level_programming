9c176e56d3d0014d2a6a48066724f3dbf8156f4d
CyrineZekri <cyrinezekri4@gmail.com>
2022-10-05 18:15:44 +0100

    alphabets

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,9 +1,17 @@
-#include"main.h"
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
 int _isalpha(int c)
 {
-    if (c = 97 && c <= 120 || c = 65 && <= 90)
+	if ((c >= 97 && c <= 122)|| (c >= 65 && c <= 90))
     return (1);
-    else
+     else
     return (0);
+    _putchar('\n');
 
 }
