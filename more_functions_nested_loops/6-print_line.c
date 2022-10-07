161dca1ba4d3f30922118577f66c48c195559a2c
CyrineZekri <cyrinezekri4@gmail.com>
2022-10-07 19:40:39 +0100

    print a line

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -7,12 +7,9 @@ void print_line(int n)
 {
 	int i, j;
 
-	for (i = 1; i <= n; i++)
+	for (j = 1; j <= i; j++)
 	{
-		for (j = 1; j <= i; j++)
-		{
-			_putchar(' ');
-		}
-		_putchar(92);
+		_putchar(' ');
 	}
+	_putchar('\n');
 }
