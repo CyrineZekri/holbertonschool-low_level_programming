5b85c0fbf086cbd42d2dbd0ba76046e45b9f619b
CyrineZekri <cyrinezekri4@gmail.com>
2022-10-07 23:41:31 +0100

    count till 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,21 +8,17 @@ void print_to_98(int n)
 {
 	int i;
 
-	i = 0;
-
 	if (n < 98)
 	{
 		for (i = n; i <= 98; i++)
-
-			printf("%d, ", i);
+		{
+			if (i != 98)
+			{
+				printf("%d, ", i);
+			}
+			else
+			{
+				printf("%d\n", i);
+			}
+		}
 	}
-
-	else if (n > 98)
-	{
-		for (i = n; i >= 98; i--)
-
-			printf("%d\n", i);
-	}
-	else
-		printf("%d\n", i);
-}
