e14dd7ea208adf13a0aea771dd642ef256206206
CyrineZekri <cyrinezekri4@gmail.com>
2022-10-07 21:30:06 +0100

    last

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -5,15 +5,24 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, x, y;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			_putchar(i / 10 + '0');
-			_putchar(i % 10 + '0');
+			if (i < 10)
+			{
+				y = i;
+				}
+			else
+			{
+				x = i / 10;
+				y = i % 10;
+				_putchar(x + '0');
+				_putchar(y + '0');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
