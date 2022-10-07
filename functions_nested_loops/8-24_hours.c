7c741f7758b3ca9ca77cc73dd56d9afdd1c14e6f
CyrineZekri <cyrinezekri4@gmail.com>
2022-10-07 23:08:32 +0100

    better bauer

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,32 +1,23 @@
 #include "main.h"
 /**
- *
+ *jack_bauer -prints time.
+ *Return: time
  */
 void jack_bauer(void)
 {
-    int x, y, i, j;
 
-    for (x = 48; x <= 50; x++)
-    {
-        for (y = 48; y <= 57; y++)
-        {
-            for (i = 48; i <= 53; i++)
-            {
-                for (j = 48; j <= 57; j++)
-                {
-                    if (x == 50 && y > 51)
-                    {
-                        continue;
-                    }
+	int i, j;
 
-                    _putchar(x);
-                    _putchar(y);
-                    _putchar(':');
-                    _putchar(i);
-                    _putchar(j);
-                    _putchar('\n');
-                }
-            }     
-        }
-    }
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 00; j < 60; j++)
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(':');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			putchar('\n');
+		}
+	}
 }
