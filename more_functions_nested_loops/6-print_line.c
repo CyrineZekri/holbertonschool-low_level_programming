e263f2a51637ea892591cfc21d57b9efc6a6bd9e
CyrineZekri <cyrinezekri4@gmail.com>
2022-10-07 19:33:40 +0100

    print a line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_line - verifies.
+ * Return: return 1 if digit or 0 if not
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			_putchar(" ");
+		}
+		_putchar(92);
+	}
