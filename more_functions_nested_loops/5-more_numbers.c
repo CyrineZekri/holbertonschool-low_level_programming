45f32d339262073f8a01cb634c04c17790c277ce
CyrineZekri <cyrinezekri4@gmail.com>
2022-10-07 19:21:13 +0100

    6th

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,9 +7,9 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (j = 0; j < 10; j++)
+	for (i = 0; i < 10; i++)
 	{
-		for (i = 0; i < 15; i++)
+		for (j = 0; j < 15; j++)
 		{
 			_putchar(i / 10 + '0');
 			_putchar(i % 10 + '0');
