cba0a795858dbfa12c81007955ee7954d15eb37f
CyrineZekri <cyrinezekri4@gmail.com>
2022-10-07 17:51:59 +0100

    task

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_isupper - s .verifies
+ * @c: - input var
+ * Return: return 1 if digit or 0 if not
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
