55e9604aa858425fd8e29b2054cef5e670c1e25d
CyrineZekri <cyrinezekri4@gmail.com>
2022-10-10 16:39:30 +0100

    a function that reverses a string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -5,21 +5,6 @@
  * Return: void.
  */
 void rev_string(char *s)
-/**{
-	int i;
-	int len = strlen(s) - 1;
-	char tmp;
-
-	for (i = 0; i <= len / 2; i++)
-	{
-		tmp = s[i];
-		s[i] = s[len];
-		s[len--] = tmp;
-		printf("%s", s);
-	}
-	printf("\n");
-}
-*/
 {
 
 	int i,
