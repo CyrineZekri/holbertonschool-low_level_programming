7cc5841c3b0f3dbffec1c9e0d6e5559ec31056ff
CyrineZekri <cyrinezekri4@gmail.com>
2022-10-10 14:05:03 +0100

    a function that prints a string

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * _strlen - s that prints a string.
+ * _puts -  that prints a string.
  *@str: input pointer
  * Return: void
  */
 void _puts(char *str)
 {
 	int i;
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
