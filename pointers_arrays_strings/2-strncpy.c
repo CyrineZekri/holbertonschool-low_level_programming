aa0928b433c3b66d56ac19f843fdae00b3a32488
CyrineZekri <cyrinezekri4@gmail.com>
2022-10-12 17:25:09 +0100

    a function that copies a string

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -9,16 +9,12 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	int len = strlen(src);
 
-	for (i = 0; dest[i] != '\0'; i++)
-		;
-	if (n > len)
-		n = len;
-	for (i = 0; src[i] && i < n; i++)
-
+	for (i = 0; dest[i] != '\0' && i < n; i++)
 	{
+
 		dest[i] = src[i];
 	}
+
 	return (dest);
 }
