cad3fc9caff660d6c51d910c4994e4557f23dae9
CyrineZekri <cyrinezekri4@gmail.com>
2022-10-14 22:47:40 +0100

    locates

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+
+#include "main.h"
+/**
+ * *_strstr - searches a string for any of a set of bytes
+ * @haystack: the address of memory to print
+ * @needle: the size of the memory to print
+ * Return: int.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+    int i, j;
+
+    for (i = 0; haystack[i] != '\0'; i++)
+    {
+        for (j = 0; haystack[i] == needle[0]; j++)
+        {
+            if (haystack[i + j] != needle[j])
+                break;
+        }
+
+        if (!needle[j])
+        {
+            return (haystack + i);
+        }
+        else
+        {
+            j = 0;
+        }
+    }
+    return (0);
+}
