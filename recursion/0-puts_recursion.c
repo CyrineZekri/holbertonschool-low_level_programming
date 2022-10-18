3bff9e6b19069c6c9e0250c58a25337b871874f4
CyrineZekri <cyrinezekri4@gmail.com>
2022-10-18 10:15:47 +0100

    a

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion -prints a string.
+ *@s: input variable
+ *Return: void.
+ */
+void _puts_recursion(char *s)
+{
+
+	if (*s == '\0')
+		_putchar('\n');
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
