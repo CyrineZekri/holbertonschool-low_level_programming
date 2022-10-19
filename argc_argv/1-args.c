508a060e7aa34cc251d589f8746307280d9ccf63
CyrineZekri <cyrinezekri4@gmail.com>
2022-10-19 16:06:20 +0100

    a

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -prints name of the program.
+ * @argc: input variablegc
+ * @argv: input variable
+ * Return:int
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
