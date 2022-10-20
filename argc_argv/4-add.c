14d17c35a437ae49d36ea450f3e4251bcc4ba059
CyrineZekri <cyrinezekri4@gmail.com>
2022-10-20 10:23:30 +0100

    a

---
+++ argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main -adds arguments.
+ * @argc: input variablegc
+ * @argv: input variable
+ * Return:int
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int result = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; j != '\0'; j++)
+			{
+				if (argv[i][j] > '9' || argv[i][j] < '0')
+					printf("Error\n");
+				return (1);
+			}
+			result += (atoi(argv[i]));
+		}
+		printf("%d\n", result);
+	}
+	return (0);
+}
