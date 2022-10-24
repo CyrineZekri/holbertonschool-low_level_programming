e5a0de481efea429b2fe5448e1a49efa86797b26
CyrineZekri <cyrinezekri4@gmail.com>
2022-10-24 19:00:20 +0100

    a

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- **_strdup-creates an identical string.
- *@str:input var
+ * *str_concat-concatenates two strings.
+ *@s1:input var
+ *@s2:input var
  *Return: a string
  */
 char *str_concat(char *s1, char *s2)
@@ -10,8 +11,7 @@ char *str_concat(char *s1, char *s2)
 	unsigned int size1 = strlen(s1);
 	unsigned int size2 = strlen(s2);
 	unsigned int sum = size1 + size2;
-	unsigned int i, j;
-	int increptr;
+	unsigned int i, j, increptr1, increptr2;
 
 	if (s1 == 0)
 		s1 = "";
@@ -19,21 +19,22 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == 0)
 		s2 = "";
 
-	str = malloc(sizeof(char) * sum + 1);
+	str = malloc(sizeof(char) * (sum + 1));
 
 	if (str == 0)
 		return (NULL);
 
-	for (i = 0; i < size1; i++)
+	for (i = 0; i < strlen(s1); i++)
 	{
-		*(str + i) = *(s1 + j);
-		j++;
+		*(str + i) = *(s1 + increptr1);
+		increptr1++;
 	}
-	for (j = i; j < sum + 1; j++)
+	for (j = i; j < sum; j++)
 	{
 
-		*(str + j) = *(s2 + increptr);
-		increptr++;
+		*(str + j) = *(s2 + increptr2);
+		increptr2++;
 	}
 	return (str);
+	free(str);
 }
