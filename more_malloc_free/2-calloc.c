109680742d4cf36e709e66fb04549e3bfcdb8fd9
CyrineZekri <cyrinezekri4@gmail.com>
2022-10-26 13:39:04 +0100

    a

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_calloc_checked-allocates memory
+ * @nmemb:input var
+ * @size:input var
+ * Return:void.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+    char *sm;
+    unsigned int i;
+
+    if (nmemb == 0 || size == 0)
+        return (NULL);
+    sm = (char *)malloc(nmemb * size);
+    if (!sm)
+        return (NULL);
+    for (i = 0; i < nmemb * size; i++)
+    {
+        sm[i] = 0;
+    }
+    return (sm);
+}
