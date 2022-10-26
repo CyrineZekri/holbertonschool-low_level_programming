249fa64de520af4eed318548c5ee3e29b8426d5d
CyrineZekri <cyrinezekri4@gmail.com>
2022-10-26 13:39:56 +0100

    a

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -7,17 +7,17 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    char *sm;
-    unsigned int i;
+	char *sm;
+	unsigned int i;
 
-    if (nmemb == 0 || size == 0)
-        return (NULL);
-    sm = (char *)malloc(nmemb * size);
-    if (!sm)
-        return (NULL);
-    for (i = 0; i < nmemb * size; i++)
-    {
-        sm[i] = 0;
-    }
-    return (sm);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	sm = (char *)malloc(nmemb * size);
+	if (!sm)
+		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+	{
+		sm[i] = 0;
+	}
+	return (sm);
 }
