2df070d2857253240cd9481a0fe06440b8b716e8
CyrineZekri <cyrinezekri4@gmail.com>
2022-10-28 17:11:22 +0100

    strlen changed

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -26,7 +26,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	len1 = strlen(s1);
 	len2 = strlen(s2);
 	j = 0;
-	sum = malloc((len1 + n) * sizeof(char));
+	sum = malloc((len1 + n + 2) * sizeof(char));
 	if (sum == NULL)
 	{
 		return (NULL);
