edb5b956d461e63bdadcbdd49f16398442c44a88
CyrineZekri <cyrinezekri4@gmail.com>
2022-10-31 10:19:21 +0100

    task0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,9 +1,15 @@
 #ifndef HEADERFILE_H
 #define HEADERFILE_H
+/**
+ * struct dog - a new structure.
+ * @name:input
+ * @age:input
+ * @owner:input
+ */
 struct dog
-{   
-    char *name;
-    float age;
-    char *owner;
+{
+	char *name;
+	float age;
+	char *owner;
 };
 #endif
