3fae72892fc26a46afa7f20ba2c3ea69e307aa5d
CyrineZekri <cyrinezekri4@gmail.com>
2022-11-06 00:00:48 +0100

    task1

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,10 @@
+#include "variadic_functions.h"
+/**
+ *sum_them_all - returns sum of inputs
+ *@n:input var
+ * Return:int.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+        
+}
