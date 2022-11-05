8f4e14b61e93ca72e2bfff80400dfc453d0bb34a
CyrineZekri <cyrinezekri4@gmail.com>
2022-11-05 21:06:21 +0100

    header file betty

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,3 +1,5 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
 #include <stdio.h>
 #include <stdlib.h>
 /**
@@ -7,9 +9,9 @@
  * @f: The function associated
  */
 typedef struct op
-{   
-    char *op;
-    int (*f)(int a, int b);
+{
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 int op_add(int a, int b);
 int op_sub(int a, int b);
@@ -17,3 +19,4 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
+#endif
