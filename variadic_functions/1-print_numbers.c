a4e75f03256c3fa0acdbe0d94cf073296b34fe9b
CyrineZekri <cyrinezekri4@gmail.com>
2022-11-06 12:30:14 +0100

    2nd task

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,10 +1,25 @@
 #include "variadic_functions.h"
 /**
- *print_numbers -prints number .
- *@seperator:input var
- *@n:input var
+ *print_numbers -prints numbers.
+ *@separator:var.
+ *@n:input var.
  * Return:void.
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	va_list liste;
+	unsigned int i = 0;
+	int x;
+
+	va_start(liste, n);
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(liste, int);
+		printf("%d", x);
+		if (i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
 }
