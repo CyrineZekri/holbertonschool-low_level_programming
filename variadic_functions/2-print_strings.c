0d351d7e80f60e09b0cc4a459beeaed4ed83ac0f
CyrineZekri <cyrinezekri4@gmail.com>
2022-11-06 12:58:16 +0100

    3rd task

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+/**
+ *print_strings -prints strings.
+ *@separator:var.
+ *@n:input var.
+ * Return:void.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list liste;
+	unsigned int i;
+	char *x;
+
+	va_start(liste, n);
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(liste, char *);
+		if (x == NULL)
+			printf("nil");
+		else
+			printf("%s", x);
+		if (i < n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+}
