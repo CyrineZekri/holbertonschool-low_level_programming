0c1272e203832f115c08b2efd7dd8d35096a772c
CyrineZekri <cyrinezekri4@gmail.com>
2022-11-07 15:24:08 +0100

    prints everything

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,73 @@
+#include "variadic_functions.h"
+/**
+ *print_char -prints all formats.
+ *@liste:input var.
+ * Return:void.
+ */
+void print_char(va_list liste)
+{
+	printf("%c", va_arg(liste, int));
+}
+/**
+ *print_int -prints all formats.
+ *@liste:var.
+ * Return:void.
+ */
+void print_int(va_list liste)
+{
+	printf("%d", va_arg(liste, int));
+}
+/**
+ *print_float -prints all formats.
+ *@liste:var.
+ * Return:void.
+ */
+void print_float(va_list liste)
+{
+	printf("%f", va_arg(liste, double));
+}
+/**
+ *print_string -prints all formats.
+ *@liste:var.
+ * Return:void.
+ */
+void print_string(va_list liste)
+{
+	printf("%s", va_arg(liste, char *));
+}
+/**
+ *print_all -prints all formats.
+ *@format:input var.
+ * Return:void.
+ */
+void print_all(const char *format, ...)
+{
+	int i = 0;
+	int j = 0;
+
+	va_list args;
+	mytypes_t types[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
+		{'\0', NULL}};
+	va_start(args, format);
+	while (format != NULL && format[i] != '\0')
+	{
+		while (types[j].s != '\0')
+		{
+			if (types[j].s == format[i])
+			{
+				types[j].f(args);
+				if (types[j + 1].s)
+					printf(", ");
+			}
+			j++;
+		}
+		i++;
+		j = 0;
+	}
+	printf("\n");
+	va_end(args);
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -4,7 +4,15 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
+typedef struct mytypes_s
+{
+    /* data */
+    char s;
+    void (*f)(va_list);
+} mytypes_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char *const format, ...);
 #endif
