fa35f4614bf3928f6cf5fb75fd56b99b1619114a
CyrineZekri <cyrinezekri4@gmail.com>
2022-11-07 10:32:53 +0100

    headerfile

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -15,9 +15,7 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog_t;
-/**
- * struct dog_h -new name for the type dog.
- */
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
