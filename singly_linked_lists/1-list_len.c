5544854424066586f46324b2a0eba4bc60aa5fbb
CyrineZekri <cyrinezekri4@gmail.com>
2022-11-16 16:29:11 +0100

    2nd task

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *list_len- prints list string elements.
+ *@h:list
+ * Return: struct.
+ */
+size_t list_len(const list_t *h)
+{
+	int c = 1;
+
+	while (h->next != NULL)
+	{
+		c++;
+		h = h->next;
+	}
+	return (c);
+}
