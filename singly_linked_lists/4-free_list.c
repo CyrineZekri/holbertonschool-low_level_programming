14de1f970a517899aece1387f30964d679226f24
CyrineZekri <cyrinezekri4@gmail.com>
2022-11-16 20:28:14 +0100

    frees list

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,13 +1,12 @@
 #include "lists.h"
 /**
- *add_node_end - adds new node.
+ *free_list -frees a list.
  *@head:pointer to struct
- *@str :string to be copied
- * Return: Address of the new node.
+ * Return:void.
  */
 void free_list(list_t *head)
 {
-    free(head->str);
-    free(head->next);
-    free(head);
+	free(head->str);
+	free(head->next);
+	free(head);
 }
