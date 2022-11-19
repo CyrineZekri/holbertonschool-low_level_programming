42f4624340ead36535772ed197e9f416e7532db6
CyrineZekri <cyrinezekri4@gmail.com>
2022-11-19 20:59:10 +0100

    free list

--- more_singly_linked_lists/4-free_listint.c
+++ more_singly_linked_lists/4-free_listint.c
@@ -6,12 +6,12 @@
  */
 void free_listint(listint_t *head)
 {
-    listint_t *ptr;
+	listint_t *ptr;
 
-    while (head != NULL)
-    {
-        ptr = head;
-        head = head->next;
-        free(ptr);
-    }
+	while (head != NULL)
+	{
+		ptr = head;
+		head = head->next;
+		free(ptr);
+	}
 }
