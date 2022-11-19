baf733248c8de213c85bd85521506e5d3d00a857
CyrineZekri <cyrinezekri4@gmail.com>
2022-11-19 22:27:43 +0100

    the mf headerfile

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -6,7 +6,7 @@
 /**
  * struct listint_s - singly linked list
  * @n: integer
- * @next: points to the next node
+ * @next: points to the next node.
  *
  * Description: singly linked list node structure
  *
@@ -21,5 +21,7 @@ size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
+void free_listint2(listint_t **head);
+int pop_listint(listint_t **head);
 int _putchar(char c);
 #endif
