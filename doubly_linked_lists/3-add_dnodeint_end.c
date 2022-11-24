983b4b267b2e415acbbe5f846edcb7f46cdbd85c
CyrineZekri <cyrinezekri4@gmail.com>
2022-11-24 20:10:00 +0100

    add node at end

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,39 +1,34 @@
 #include "lists.h"
 /**
- *add_dnodeint-adds node at beginning.
+ *add_dnodeint_end-adds node at beginning.
  *@head: head of the list.
  *@n:int.
  *  Return:address of the new element.
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *newnode, *ptr;
+	dlistint_t *newnode, *ptr;
 
-    newnode = malloc(sizeof(dlistint_t));
-    if (newnode == NULL)
-    {
-        return (NULL);
-    }
-    newnode->next = NULL;
-    newnode->n = n;
-    if (head == NULL)
-    {
-        newnode->prev = NULL;
-        *head = newnode;
-        return (newnode);
-    }
-    ptr = *head;
-    while (ptr)
-    {
-        if (ptr->next == NULL)
-        {
-            ptr->next = newnode;
-            newnode->prev = ptr;
-        }
-        else
-        {
-            ptr = ptr->next;
-        }
-    }
-    return (ptr);
+	newnode = malloc(sizeof(dlistint_t));
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+	newnode->next = NULL;
+	newnode->n = n;
+	if (*head == NULL)
+	{
+		newnode->prev = NULL;
+		*head = newnode;
+		return (newnode);
+	}
+	ptr = *head;
+	while (ptr->next)
+	{
+
+		ptr = ptr->next;
+	}
+	ptr->next = newnode;
+	newnode->prev = ptr;
+	return (ptr);
 }
