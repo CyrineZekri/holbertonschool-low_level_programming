639e8f843b11866cdce53b1b57a1da059ceff8df
CyrineZekri <cyrinezekri4@gmail.com>
2022-11-24 19:10:01 +0100

    add at beginning

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,34 +1,30 @@
 #include "lists.h"
 /**
  *add_dnodeint-adds node at beginning.
- *@h: head of the list.
- * Return:address of the new element.
+ *@head: head of the list.
+ *@n:int.
+ *  Return:address of the new element.
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *newnode;
+	dlistint_t *newnode, *ptr;
 
-    newnode = (dlistint_t *)malloc(sizeof(dlistint_t));
-    if (newnode == NULL)
-    {
-        return (NULL);
-    }
-    /**
-    if (head == NULL)
-    {
-        newnode->n = n;
-        newnode->prev = NULL;
-        newnode->next = NULL;
-        newnode = *head;
-    }
-*/
-    while (*head)
-    {
-        newnode->n = n;
-        newnode->prev = NULL;
-        newnode->next = *head;
-        *head = newnode;
-        (*head)->prev = newnode;
-    }
-    return (*head);
+	newnode = (dlistint_t *)malloc(sizeof(dlistint_t));
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+	ptr = *head;
+
+	newnode->n = n;
+	newnode->prev = NULL;
+	newnode->next = (*head);
+	if (ptr != NULL)
+	{
+
+		(*(head))->prev = newnode;
+	}
+	*head = newnode;
+
+	return (*head);
 }
