efaa78fb43daffd57f4a6659cf285753b24ee4a6
CyrineZekri <cyrinezekri4@gmail.com>
2022-11-24 20:25:00 +0100

    free list

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *free_dlistint-freeslist.
+ *@head: head of the list.
+ *  Return:void.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *ptr;
+
+	while (head != NULL)
+	{
+		ptr = head;
+		head = head->next;
+		free(ptr);
+	}
+}
