3458ea65f00fa062c529c6a86809bfd05e663a28
CyrineZekri <cyrinezekri4@gmail.com>
2022-11-26 21:16:59 +0100

    file

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -3,31 +3,31 @@
  * read_textfile-reads text and prints it to POSX
  * @filename:file to be opened
  * @letters:number of letters to be printed
- * Return:nbr of letters that could be printed.
+ * Return:nbr of letters that could conte printed.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int f;
-    size_t i;
-    char *b;
+	int file;
+	size_t i;
+	char *str;
 
-    if (filename == NULL)
-    {
-        return (0);
-    }
-    f = open(filename, O_RDONLY);
-    if (f == -1)
-    {
-        return (0);
-    }
-    b = malloc(letters);
-    if (b == NULL)
-    {
-        return (0);
-    }
-    i = read(f, b, letters);
-    b[i] = '\0';
-    write(STDOUT_FILENO, b, i);
-    close(f);
-    return (i);
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+	{
+		return (0);
+	}
+	str = malloc(letters);
+	if (cont == NULL)
+	{
+		return (0);
+	}
+	i = read(file, str, letters);
+	cont[i] = '\0';
+	write(STDOUT_FILENO, str, i);
+	close(file);
+	return (i);
 }
