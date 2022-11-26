c4cc00f5274ddb6cb6f2caef3c174656fd736439
CyrineZekri <cyrinezekri4@gmail.com>
2022-11-26 23:22:11 +0100

    hash

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+/**
+ *hash_table_create-creates hash table
+ *@size:size of the hash table.
+ * Return: table created.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hashtable;
+
+	hashtable = malloc(size * sizeof(hash_table_t));
+	if (hashtable == NULL)
+	{
+		return (NULL);
+	}
+	hashtable->size = size;
+	hashtable->array = malloc(sizeof(hash_node_t) * size);
+	if (hashtable->array == NULL)
+	{
+		return (NULL);
+	}
+	return (hashtable);
+}
