6fda6ce2cc176ece2c8cf56e99d6d9db6cc3aac4
CyrineZekri <cyrinezekri4@gmail.com>
2022-11-26 22:57:20 +0100

    test

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -7,22 +7,22 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int file, len;
-    if (filename == NULL)
-    {
-        return (-1);
-    }
-    file = open(filename, O_APPEND | O_WRONLY);
-    if (file == -1)
-    {
-        return (-1);
-    }
-    if (text_content == NULL)
-    {
-        return (1);
-    }
-    len = strlen(text_content);
-    write(file, text_content, len);
-    close(file);
-    return (1);
+	int file, len;
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	file = open(filename, O_APPEND | O_WRONLY);
+	if (file == -1)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		return (1);
+	}
+	len = strlen(text_content);
+	write(file, text_content, len);
+	close(file);
+	return (1);
 }
