54ea57d6839b2e874bea035307b4b47a07479bfb
CyrineZekri <cyrinezekri4@gmail.com>
2022-11-26 23:39:17 +0100

    header file

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -8,10 +8,9 @@
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 
-    unsigned long int index, hash;
+	unsigned long int index, hash;
 
-    hash = hash_djb2(key);
-    index = hash % size;
-
-    return (index);
+	hash = hash_djb2(key);
+	index = hash % size;
+	return (index);
 }
