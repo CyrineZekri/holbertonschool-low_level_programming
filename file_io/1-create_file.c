23663ed5a41b64daeef94939cbed434625a70ee8
CyrineZekri <cyrinezekri4@gmail.com>
2022-11-26 22:24:52 +0100

    betty

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,20 +9,20 @@ int create_file(const char *filename, char *text_content)
 {
     int f, x, l;
 
-	f = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
-	if (f == -1)
-	{
-		return (-1);
-	}
-	if (text_content == NULL && f != -1)
-	{
-		return (1);
-	}
-	l = strlen(text_content);
-	x = write(f, text_content, l);
-	if (x == -1)
-	{
-		return (-1);
-	}
-	return (1);
+    f = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+    if (f == -1)
+    {
+        return (-1);
+    }
+    if (text_content == NULL && f != -1)
+    {
+        return (1);
+    }
+    l = strlen(text_content);
+    x = write(f, text_content, l);
+    if (x == -1)
+    {
+        return (-1);
+    }
+    return (1);
 }
