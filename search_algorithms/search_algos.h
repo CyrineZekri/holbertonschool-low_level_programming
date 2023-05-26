e25c3321b14fa2821717f38b315f0a93ef6c7d21
CyrineZekri <cyrinezekri4@gmail.com>
2023-05-26 20:40:13 +0100

    linear search

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,7 @@
+#ifndef INV_TREE_H
+#define INV_TREE_H
+#include <stdio.h>
+#include <stdlib.h>
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+#endif
